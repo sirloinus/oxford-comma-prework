bffd302861ea9d616d2352293eb6588f5d956fe2
Amalie S <amaliesmidth@gmail.com>
2018-08-27 20:28:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2 
     array.join(" and ")
   elsif array.length == 3 
-    array.join(", ")
-    
+    sentence = array.join(", ")
+    sentence.reverse.sub(',', '& ').reverse
   else 
     array.join(", ")
   end
