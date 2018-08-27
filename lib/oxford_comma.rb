dbcdb506274b4dbf42278dc2feefc0d0effc88c8
Amalie S <amaliesmidth@gmail.com>
2018-08-27 20:27:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length == 3 
+    array.join(", ")
+    
+  else 
+    array.join(", ")
+  end
 end
+
+# puts "#{sentence[0]}, #{sentence[1]} and #{sentence[2]}"
+
+
+# it "adds 'and' between elements when given a 2-element array" do
+#     expect(oxford_comma(["kiwi", "durian"])).to eq("kiwi and durian")
+#   end
+#   it "adds commas plus a final 'and' when given a 3-element array" do
+#     expect(oxford_comma(["kiwi", "durian", "starfruit"])).to eq("kiwi, durian, and starfruit")
+#   end
+#   it "correctly formats arrays of lengths greater than three" do
+#     expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])).to eq("kiwi, durian, starfruit, mangos, and dragon fruits")
+#     expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])).to eq("kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos")
+#   end
+# end
