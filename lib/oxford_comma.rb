de14f59139fbdd16c881fdeeaa452a83bd1dd2e4
Amalie S <amaliesmidth@gmail.com>
2018-08-27 20:30:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length == 3 
+    sentence = array.join(", ")
+    sentence.reverse.sub(',', 'dna ,').reverse
+  else 
+    sentence = array.join(", ")
+    sentence.reverse.sub(',', 'dna ,').reverse
+  end
 end
+
